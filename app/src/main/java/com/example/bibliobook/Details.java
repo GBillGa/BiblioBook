2eeb8c33f37d15dbf93c4333a39a9367357a66d6
BILLES GARABEDIAN Guillaume <guillaume.billes.garabedian@efrei.net>
2019-04-09 01:22:23 +0200

    listview + details

--- app/src/main/java/com/example/bibliobook/Details.java
+++ app/src/main/java/com/example/bibliobook/Details.java
@@ -1,13 +1,11 @@
 package com.example.bibliobook;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -16,6 +14,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -23,22 +22,20 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 
-public class ResultList extends AppCompatActivity {
+public class Details extends AppCompatActivity {
 
     private RequestQueue queue;
-    private ListView mListView;
-    private ArrayList<Livre> livreArrayList = new ArrayList<>();
-    private ArrayList<String> idLivre = new ArrayList<>();
-
+    private String idLExt = "";
+    private String pageNumberExt = "No";
+    private String descriptionExt = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_result_list);
+        setContentView(R.layout.activity_details);
         queue = Volley.newRequestQueue(this);
-        final TextView text = (TextView) findViewById(R.id.TopBarSearchText);
-        String searchedTerms = getIntent().getStringExtra("SEARCHED");
-        text.setText(searchedTerms);
+        String titreExtra = getIntent().getStringExtra("TITRE");
+        String idExtra = getIntent().getStringExtra("ID");
         ImageView imageview1 = findViewById(R.id.returnToSearchArrowG);
         imageview1.setOnClickListener(new View.OnClickListener() {
 
@@ -51,59 +48,70 @@ public class ResultList extends AppCompatActivity {
             }
 
         });
-        //Log.e("JFL", "ici");
-        //final TextView textCenter = (TextView) findViewById(R.id.textView3);
-        String urlC = "https://www.googleapis.com/books/v1/volumes?q=" + searchedTerms;
-        Log.e("JFL", urlC);
-        mListView = findViewById(R.id.listView);
+        TextView toolbarText = findViewById(R.id.TopBarSearchTextG);
+        final TextView titreText = findViewById(R.id.titreG);
+        final TextView auteurText = findViewById(R.id.auteurG);
+        final TextView genreText = findViewById(R.id.genreG);
+        final TextView anneeText = findViewById(R.id.anneeG);
+        final TextView pageText = findViewById(R.id.pageG);
+        final TextView descText = findViewById(R.id.descriptionG);
+        final ImageView couvertureImg = findViewById(R.id.couvertureG);
+        toolbarText.setText("Details");
+        String urlC = "https://www.googleapis.com/books/v1/volumes?q=" + idExtra + "\"+intitle:\"" + titreExtra + "\"";
+        final Livre livreInfo = new Livre("","","","","");
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
                 (Request.Method.GET, urlC, null, new Response.Listener<JSONObject>() {
 
                     @Override
                     public void onResponse(JSONObject response) {
-
+                        //              Log.e("JFL", "Reponse");
 
                         try {
                             JSONArray jsonArray = response.getJSONArray("items");
-            //                Log.e("JFL", "jsonArray:" + jsonArray);
                             for (int i = 0; i < jsonArray.length(); i++) {
                                 JSONObject livre = jsonArray.getJSONObject(i);
                                 JSONObject info = livre.getJSONObject("volumeInfo");
-                                String titre = info.getString("title");
-                                //Log.i("JFL", "info:" + info);
-                                String auteur = "No author";
+                                livreInfo.setTitre(info.getString("title"));
+                                livreInfo.setAuteur("No author");
                                 if (info.has("authors")) {
                                     JSONArray auteurArray = info.getJSONArray("authors");
-                                    auteur = (String) auteurArray.get(0);
+                                    livreInfo.setAuteur((String) auteurArray.get(0));
                                 }
-                                String genre = "No genre";
+                                livreInfo.setGenre("No genre");
                                 if (info.has("categories")) {
                                     JSONArray genreArray = info.getJSONArray("categories");
-                                    genre = (String)genreArray.get(0);
+                                    livreInfo.setGenre((String)genreArray.get(0));
                                 }
-                                String date = "No publication date";
+                                livreInfo.setAnnee("No publication date");
                                 if (info.has("publishedDate")){
-                                    date = info.getString("publishedDate");
+                                    livreInfo.setGenre(info.getString("publishedDate"));
                                 }
-                                String idL = "";
                                 if (livre.has("id")){
-                                    idL = livre.getString("id");
+                                    idLExt = livre.getString("id");
+                                }
+                                if (info.has("pageCount")){
+                                    pageNumberExt = info.getString("pageCount");
+                                }
+                                if (info.has("description")){
+                                    descriptionExt = info.getString("description");
                                 }
                                 String miniature = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png";
+                                livreInfo.setUrl(miniature);
                                 if (info.has("imageLinks")){
                                     JSONObject images = info.getJSONObject("imageLinks");
-                                    miniature = images.getString("thumbnail");
+                                    livreInfo.setUrl(images.getString("thumbnail"));
                                 }
-                                Livre livreLoop = new Livre(titre,auteur,genre,date,miniature);
-                                livreArrayList.add(livreLoop);
-                                idLivre.add(idL);
                             }
-                            LivreAdapter adapter = new LivreAdapter(ResultList.this, livreArrayList);
-                            Log.i("JFL", "info:" + adapter);
-                            mListView.setAdapter(adapter);
+                            titreText.setText(livreInfo.getTitre());
+                            auteurText.setText(livreInfo.getAuteur());
+                            genreText.setText(livreInfo.getGenre());
+                            anneeText.setText(livreInfo.getAnnee());
+                            pageText.setText(pageNumberExt + " pages");
+                            descText.setText(descriptionExt);
+                            descText.setMovementMethod(new ScrollingMovementMethod());
+                            Picasso.get().load(livreInfo.getUrl()).into(couvertureImg);
                         } catch (JSONException e) {
                             e.printStackTrace();
-                            Log.e("JFL","JSON Parsing Failed");
                             //textCenter.setText("JSON Parsing Failed");
                         }
                     }
@@ -117,20 +125,5 @@ public class ResultList extends AppCompatActivity {
                     }
                 });
         queue.add(jsonObjectRequest);
-
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Livre item = LivreAdapter.getItemC(livreArrayList,position);
-                String titre = item.getTitre();
-                String idLivreClicked = idLivre.get(position);
-                Intent intent = new Intent(getBaseContext(), Details.class);
-                intent.putExtra("TITRE", titre);
-                intent.putExtra("ID", idLivreClicked);
-                //based on item add info to intent
-                startActivity(intent);
-            }
-        });
     }
-
 }
--- app/src/main/java/com/example/bibliobook/LivreAdapter.java
+++ app/src/main/java/com/example/bibliobook/LivreAdapter.java
@@ -1,77 +1,69 @@
 package com.example.bibliobook;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
-import java.util.ArrayList;
+import java.util.List;
 
-public class LivreAdapter extends RecyclerView.Adapter<LivreAdapter.ViewHolder> {
+public class LivreAdapter extends ArrayAdapter<Livre> {
 
-    private ArrayList<Livre> mDataset;
-
-    public LivreAdapter(ArrayList<Livre> mDataset) {
-        this.mDataset = mDataset;
-
-        Log.d("TESTITO","ok" + mDataset.size());
+    //tweets est la liste des models à afficher
+    public LivreAdapter(Context context, List<Livre> livres) {
+        super(context, 0, livres);
     }
 
-
-    @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.card,viewGroup,false);
-        ViewHolder vh = new ViewHolder(v);
-        Log.d("TESTITO","onCreateViewHolder" + viewGroup.toString());
-        return vh;
+    public static Livre getItemC(List<Livre> livres,int position){
+        return livres.get(position);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
-        viewHolder.titre.setText(mDataset.get(i).getTitre());
-        viewHolder.auteur.setText(mDataset.get(i).getAuteur());
-        viewHolder.genre.setText(mDataset.get(i).getGenre());
-        viewHolder.annee.setText(mDataset.get(i).getAnnee());
-        Picasso.get().load(mDataset.get(i).getUrl()).into(viewHolder.miniature);
-        Log.d("TESTITO","onBindViewHolder");
-    }
+    public View getView(int position, View convertView, ViewGroup parent) {
 
-    @Override
-    public int getItemCount() {
-        Log.d("TESTITO","getItemCount" + mDataset.size());
-        return mDataset.size();
-    }
-
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-
-        private TextView titre = null;
-        private TextView auteur = null;
-        private TextView genre = null;
-        private TextView annee = null;
-        private ImageView miniature = null;
-
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
-            if (titre == null){
-                Log.d("TESTITO","TITRE EST NUL AU DEBUT");
-            }
-            this.titre = itemView.findViewById(R.id.title);
-            this.auteur = itemView.findViewById(R.id.auteur);
-            this.genre = itemView.findViewById(R.id.genre);
-            this.annee = itemView.findViewById(R.id.annee);
-            this.miniature = itemView.findViewById(R.id.couverture);
-
-            if (titre == null){
-                Log.d("TESTITO","TITRE EST NUL A LA FIN");
-            }
-
+        if(convertView == null){
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.card,parent, false);
         }
+
+        LivreViewHolder viewHolder = (LivreViewHolder) convertView.getTag();
+        if(viewHolder == null){
+            viewHolder = new LivreViewHolder();
+            viewHolder.titre = (TextView) convertView.findViewById(R.id.titre);
+            viewHolder.auteur = (TextView) convertView.findViewById(R.id.auteur);
+            viewHolder.genre = (TextView) convertView.findViewById(R.id.genre);
+            viewHolder.annee = (TextView) convertView.findViewById(R.id.annee);
+            viewHolder.couverture = (ImageView) convertView.findViewById(R.id.couverture);
+            convertView.setTag(viewHolder);
+        }
+
+        //getItem(position) va récupérer l'item [position] de la List<Tweet> tweets
+        Livre livre = getItem(position);
+
+        //il ne reste plus qu'à remplir notre vue
+        viewHolder.titre.setText(livre.getTitre());
+        viewHolder.annee.setText(livre.getAnnee());
+        viewHolder.auteur.setText(livre.getAuteur());
+        viewHolder.genre.setText(livre.getGenre());
+        if (livre.getUrl() != null){
+            Picasso.get().load(livre.getUrl()).into(viewHolder.couverture);
+        }
+
+        return convertView;
+    }
+
+
+
+    class LivreViewHolder{
+        public TextView titre;
+        public TextView auteur;
+        public TextView genre;
+        public TextView annee;
+        public ImageView couverture;
     }
 }
+
